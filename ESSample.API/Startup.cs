13600f5378627e38cf7191ca95aabb3470acfd53
m_jalali <m_jalali@IAUEC-IT19>
2021-04-11 11:00:40 +0430

    second commit

---
+++ ESSample.API/Startup.cs
@@ -0,0 +1,89 @@
+﻿namespace ESSample.API
+{
+    using API.Domain.Policies;
+    using API.Infrastructure;
+    using Application.Meetup.Commands.CreateMeetup;
+    using Core;
+    using ESSample.Domain.MeetupAggregate.Policies;
+    using ESSample.Infrastructure;
+    using MediatR;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.OpenApi.Models;
+    using System.Reflection;
+    //using Microsoft.AspNetCore.Builder;
+
+    public class Startup
+    {
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CreateMeetupCommand).GetTypeInfo().Assembly);
+
+            var domainEventMapper = new DomainEventMapper();
+            domainEventMapper.Map();
+
+            var documentStoreInitializer = new DocumentStoreInitializer(
+                url: Configuration.GetValue<string>("RavenDB:Url"),
+                databaseName: Configuration.GetValue<string>("RavenDB:DatabaseName"));
+
+            var documentStore = documentStoreInitializer.GetDocumentStore();
+
+            var elasticClientInitializer = new ElasticClientInitializer(
+                uri: Configuration.GetValue<string>("ElasticSearch:Uri"));
+
+            var elasticClient = elasticClientInitializer.GetElasticClientAsync().GetAwaiter().GetResult();
+
+            var eventStoreConnectionInitializer = new EventStoreConnectionInitializer(
+               connectionString: Configuration.GetValue<string>("EventStore:ConnectionString"),
+               connectionName: Configuration.GetValue<string>("EventStore:ConnectionName"));
+
+            var eventStoreConnection = eventStoreConnectionInitializer.GetEventStoreConnectionAsync().GetAwaiter().GetResult();
+
+            services.AddSingleton(documentStore);
+            services.AddSingleton(elasticClient);
+            services.AddSingleton(eventStoreConnection);
+            services.AddSingleton<IDomainEventMapper>(domainEventMapper);
+            services.AddSingleton<IMeetupPolicy, MeetupPolicy>();
+            services.AddSingleton<IAggregateRepository, AggregateRepository>();
+
+            services.AddMvc();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CQRS_MediatR", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CQRS_MediatR v1"));
+            }
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# EventSourcing_DotnetCore5-_Meetup
+## Event Sourcing - CQRS
+
+### Used technologies
+Docker  
+EventStore  
+RavenDB 
+ElasticSearch  
+
+> docker-compose up
+
+#### Aggregate Roots
+Meetup
+
+#### Domain Events
+MeetupRegistered  
+MeetupSubjectChanged  
+MeetupWhenChanged  
+MeetupDescriptionChanged  
+MeetupAddressChanged  
+MeetupJoined  
+MeetupPhotoAdded  
+MeetupCommentAdded  
+MeetupCompleted  
+MeetupCancelled 
+
+#### Domain Services
+MeetupPolicy
+
+#### Projections
+QueryProjection  
+AutoCompleteProjection
